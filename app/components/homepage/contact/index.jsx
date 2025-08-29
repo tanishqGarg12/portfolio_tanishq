35a14d8b7acf6e0d2dadef98a7a98dc51b6b934b
Rajat Sharma <rajat.sharma@soprasteria.com>
2025-08-29 18:43:50 +0530

    added whatsapp in contact

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -3,9 +3,9 @@ import { personalData } from '@/utils/data/personal-data';
 import Link from 'next/link';
 import { BiLogoLinkedin } from "react-icons/bi";
 import { CiLocationOn } from "react-icons/ci";
-import { FaFacebook, FaStackOverflow } from 'react-icons/fa';
+import { FaFacebook, FaStackOverflow, FaWhatsapp } from 'react-icons/fa';
 import { FaInstagram, FaXTwitter } from "react-icons/fa6";
-import { IoLogoGithub, IoMdCall } from "react-icons/io";
+import { IoLogoGithub, IoLogoWhatsapp, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
 import ContactForm from './contact-form';
 
@@ -38,6 +38,15 @@ function ContactSection() {
                 {personalData.phone}
               </span>
             </p>
+             <p className="text-sm md:text-xl flex items-center gap-3">
+              <IoLogoWhatsapp
+                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#1c9935] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={36}
+              />
+              <span>
+                {personalData.whatsApp}
+              </span>
+            </p>
             <p className="text-sm md:text-xl flex items-center gap-3">
               <CiLocationOn
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#ab0f17] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
@@ -52,37 +61,37 @@ function ContactSection() {
             <Link target="_blank" href={personalData.github}>
               <IoLogoGithub
                 className="transition-all text-[#00FFFF] hover:scale-125 duration-300"
-                size={40}
+                size={36}
               />
             </Link>
             <Link target="_blank" href={personalData.linkedIn}>
               <BiLogoLinkedin
                 className="transition-all text-[#00FFFF] hover:scale-125 duration-300"
-                size={40}
+                size={36}
               />
             </Link>
             <Link target="_blank" href={personalData.twitter}>
               <FaXTwitter
                 className="transition-all text-[#00FFFF] hover:scale-125 duration-300"
-                size={40}
+                size={36}
               />
             </Link>
-            {/* <Link target="_blank" href={personalData.stackOverflow}>
-              <FaStackOverflow
-                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={48}
+            <Link target="_blank" href={personalData.whatsAppUrl}>
+              <FaWhatsapp
+                className="hover:scale-125 text-[#00FFFF] transition-all duration-300"
+                size={36}
               />
-            </Link> */}
+            </Link>
             <Link target="_blank" href={personalData.facebook}>
               <FaFacebook
                 className="transition-all text-[#00FFFF] hover:scale-125 duration-300"
-                size={40}
+                size={36}
               />
             </Link>
             <Link target="_blank" href={personalData.instagram}>
               <FaInstagram
                 className="transition-all text-[#00FFFF] hover:scale-125 duration-300"
-                size={40}
+                size={36}
               />
             </Link>
           </div>
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -6,12 +6,14 @@ export const personalData = {
   description: "Hi Everyone, I am RAJAT SHARMA. \n I’m a dedicated Web Developer with a strong passion for crafting intuitive, high-performance web applications. With hands-on experience in modern JavaScript frameworks like React and Vue, I specialize in building responsive, scalable, and user-friendly interfaces that bring ideas to life. \n Whether it's creating a dynamic frontend or collaborating with teams to deliver impactful solutions, I take pride in building with purpose and precision—always pushing to grow, improve, and make a meaningful contribution through technology.",
   email: 'rajatsharma221098@gmail.com',
   phone: '+91-9873957498',
+  whatsApp: '+91-9454276383',
   address: 'Greater Noida, India',
   github: 'https://github.com/Rajat-22',
   facebook: 'https://www.facebook.com/profile.php?id=100009971371239',
   linkedIn: 'https://www.linkedin.com/in/rajatsharma22',
   twitter: 'https://twitter.com/_mr_Rajat_?s=09',
   instagram: 'https://www.instagram.com/_mr__sharma__/',
+  whatsAppUrl: 'https://wa.me/+919454276383?text=Hello%20I%20want%20to%20connect%20with%20you',
   devUsername: "mrsharma",
   resume: "https://www.linkedin.com/in/rajatsharma22"
 }
